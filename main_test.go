9ad1c72e4486eabfbd26f38ab3fe8af4b68e43f2
Iovesophy <15680172+Iovesophy@users.noreply.github.com>
2021-06-05 02:14:55 +0900

    ADD Example Test on main_test.go

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 	"reflect"
@@ -9,35 +8,20 @@ import (
 	"testing"
 )
 
-func ExtractStdout(t *testing.T, fnc func()) string {
-	t.Helper()
-	orgStdout := os.Stdout
-	defer func() {
-		os.Stdout = orgStdout
-	}()
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-	fnc()
-	w.Close()
-	var buf bytes.Buffer
-	if _, err := buf.ReadFrom(r); err != nil {
-		t.Fatalf("failed to read buf: %v", err)
-	}
-	s := buf.String()
-	// Pipeを使用した際にbuf末尾に改行が追加される問題の対処のため
-	return s[:len(s)-1]
-}
-
 const count int = 100
 
 func TestPrintTail(t *testing.T) {
+	ExamplePrintTailFile()
+	ExamplePrintTailStdin()
+}
+
+func ExamplePrintTailFile() {
 	stream, err := os.Open("test.txt")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 	defer stream.Close()
-
 	tf := &fileTail{
 		nArg:         1,
 		filename:     "test.txt",
@@ -47,24 +31,49 @@ func TestPrintTail(t *testing.T) {
 		},
 	}
 	tf.appendQueue(stream)
-	actualAll := ExtractStdout(t, tf.stdinTail.printTail)
-	expectedAll := "test91\ntest92\ntest93\ntest94\ntest95\ntest96\ntest97\ntest98\ntest99\ntest100"
-	if reflect.DeepEqual(actualAll, expectedAll) {
-		t.Log(reflect.DeepEqual(actualAll, expectedAll))
-	} else {
-		t.Errorf("got %v\nwant %v", actualAll, expectedAll)
+	tf.stdinTail.printTail()
+	// Output:
+	// test91
+	// test92
+	// test93
+	// test94
+	// test95
+	// test96
+	// test97
+	// test98
+	// test99
+	// test100
+}
+
+func ExamplePrintTailStdin() {
+	stream, err := os.Open("test.txt")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
-	// stdin ver check
+	defer stream.Close()
+	tf := &fileTail{
+		nArg:         1,
+		filename:     "test.txt",
+		isNotEndFile: false,
+		stdinTail: stdinTail{
+			maxQueueSize: 10,
+		},
+	}
 	os.Stdin = stream
 	tf.appendQueue(os.Stdin)
-	actualAll = ExtractStdout(t, tf.printTail)
-	expectedAll = "test91\ntest92\ntest93\ntest94\ntest95\ntest96\ntest97\ntest98\ntest99\ntest100"
-	if reflect.DeepEqual(actualAll, expectedAll) {
-		t.Log(reflect.DeepEqual(actualAll, expectedAll))
-	} else {
-		t.Errorf("got %v\nwant %v", actualAll, expectedAll)
-	}
 	tf.printTail()
+	// Output:
+	// test91
+	// test92
+	// test93
+	// test94
+	// test95
+	// test96
+	// test97
+	// test98
+	// test99
+	// test100
 }
 
 func TestAppendQueue(t *testing.T) {
@@ -82,34 +91,12 @@ func TestAppendQueue(t *testing.T) {
 		}
 		stream, err := os.Open(testAppendQueueForFileTail.filename)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err)
+			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
 		defer stream.Close()
 		testAppendQueueForFileTail.stdinTail.appendQueue(stream)
 		actualAll = testAppendQueueForFileTail.stdinTail.queueData
-		expectedAll = append([]string{"test" + strconv.Itoa(i)}, expectedAll...)
-		if reflect.DeepEqual(actualAll, expectedAll) {
-			t.Log(reflect.DeepEqual(actualAll, expectedAll))
-		} else {
-			t.Errorf("got %v\nwant %v", actualAll, expectedAll)
-		}
-	}
-	// stdin ver check
-	expectedAll = []string{}
-	for i, nAll := count, 1; i > 0; i, nAll = i-1, nAll+1 {
-		testAppendQueueForStdinTail := &stdinTail{
-			maxQueueSize: nAll,
-		}
-		stream, err := os.Open("test.txt")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err)
-			os.Exit(1)
-		}
-		defer stream.Close()
-		os.Stdin = stream
-		testAppendQueueForStdinTail.appendQueue(os.Stdin)
-		actualAll = testAppendQueueForStdinTail.queueData
 		expectedAll = append([]string{"test" + strconv.Itoa(i)}, expectedAll...)
 		if reflect.DeepEqual(actualAll, expectedAll) {
 			t.Log(reflect.DeepEqual(actualAll, expectedAll))
@@ -133,7 +120,7 @@ func TestDoTail(t *testing.T) {
 	}
 	fp, err := os.Open("test.txt")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 	defer fp.Close()
@@ -150,86 +137,105 @@ func TestDoTail(t *testing.T) {
 }
 
 func TestXOpen(t *testing.T) {
-	var actualAll []string
-	var expectedAll []string
-	testXOpen := &fileTail{
-		nArg:         1,
-		filename:     "test.txt",
-		isNotEndFile: false,
-		stdinTail: stdinTail{
-			maxQueueSize: 10,
-		},
-	}
-	stream := xOpen("test.txt")
-	doTail(testXOpen, stream)
-	actualAll = testXOpen.queueData
-	fp, err := os.Open("test.txt")
+	ExampleXOpen()
+}
+
+func ExampleXOpen() {
+	os.Stdin = xOpen("test.txt")
+	backup := os.Args
+	os.Args = []string{"serial", "-n", "10"}
+	main()
+	// Output:
+	// test91
+	// test92
+	// test93
+	// test94
+	// test95
+	// test96
+	// test97
+	// test98
+	// test99
+	// test100
+	os.Args = backup
+}
+
+func TestMain(t *testing.T) {
+	ExampleMainDefault()
+	ExampleMainMultipleFile()
+	ExampleMainStdin()
+}
+
+func ExampleMainDefault() {
+	backup := os.Args
+	os.Args = []string{"serial", "-n", "10", "./test.txt"}
+
+	main()
+	// Output:
+	// test91
+	// test92
+	// test93
+	// test94
+	// test95
+	// test96
+	// test97
+	// test98
+	// test99
+	// test100
+	os.Args = backup
+}
+
+func ExampleMainMultipleFile() {
+	backup := os.Args
+	os.Args = []string{"serial", "-n", "10", "./test.txt", "./test.txt"}
+	main()
+	// Output:
+	// ==> ./test.txt <==
+	// test91
+	// test92
+	// test93
+	// test94
+	// test95
+	// test96
+	// test97
+	// test98
+	// test99
+	// test100
+	//
+	// ==> ./test.txt <==
+	// test91
+	// test92
+	// test93
+	// test94
+	// test95
+	// test96
+	// test97
+	// test98
+	// test99
+	// test100
+	os.Args = backup
+}
+
+func ExampleMainStdin() {
+	fp, err := os.Open("./test.txt")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 	defer fp.Close()
-	doTail(testXOpen, fp)
-	expectedAll = testXOpen.queueData
-	if reflect.DeepEqual(actualAll, expectedAll) {
-		t.Log(reflect.DeepEqual(actualAll, expectedAll))
-	} else {
-		t.Errorf("got %v\nwant %v", actualAll, expectedAll)
-	}
-}
-
-func TestMain(m *testing.T) {
-	m.Run("default", func(m *testing.T) {
-		backupArgs := os.Args
-		testArgs := []string{"serial", "-n", "10", "./test.txt"}
-		os.Args = testArgs
-		m.Log(os.Args)
-		main()
-		actualAll := ExtractStdout(m, main)
-		expectedAll := "test91\ntest92\ntest93\ntest94\ntest95\ntest96\ntest97\ntest98\ntest99\ntest100"
-		if reflect.DeepEqual(actualAll, expectedAll) {
-			m.Log(reflect.DeepEqual(actualAll, expectedAll))
-		} else {
-			m.Errorf("got %v\nwant %v", actualAll, expectedAll)
-		}
-		os.Args = backupArgs
-	})
-
-	m.Run("files", func(m *testing.T) {
-		backupArgs := os.Args
-		testArgs := []string{"serial", "-n", "10", "./test.txt", "./test.txt"}
-		os.Args = testArgs
-		m.Log(os.Args)
-		main()
-		actualAll := ExtractStdout(m, main)
-		expectedAll := "==> ./test.txt <==\ntest91\ntest92\ntest93\ntest94\ntest95\ntest96\ntest97\ntest98\ntest99\ntest100\n\n==> ./test.txt <==\ntest91\ntest92\ntest93\ntest94\ntest95\ntest96\ntest97\ntest98\ntest99\ntest100"
-		if reflect.DeepEqual(actualAll, expectedAll) {
-			m.Log(reflect.DeepEqual(actualAll, expectedAll))
-		} else {
-			m.Errorf("got %v\nwant %v", actualAll, expectedAll)
-		}
-		os.Args = backupArgs
-	})
-
-	m.Run("stdin", func(m *testing.T) {
-		backupArgs := os.Args
-		fp, err := os.Open("./test.txt")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err)
-			os.Exit(1)
-		}
-		defer fp.Close()
-		os.Stdin = fp
-		testArgs := []string{"serial", "-n", "10"}
-		os.Args = testArgs
-		m.Log(os.Args)
-		actualAll := ExtractStdout(m, main)
-		expectedAll := "test91\ntest92\ntest93\ntest94\ntest95\ntest96\ntest97\ntest98\ntest99\ntest100"
-		if reflect.DeepEqual(actualAll, expectedAll) {
-			m.Log(reflect.DeepEqual(actualAll, expectedAll))
-		} else {
-			m.Errorf("got %v\nwant %v", actualAll, expectedAll)
-		}
-		os.Args = backupArgs
-	})
+	os.Stdin = fp
+	backup := os.Args
+	os.Args = []string{"serial", "-n", "10"}
+	main()
+	// Output:
+	// test91
+	// test92
+	// test93
+	// test94
+	// test95
+	// test96
+	// test97
+	// test98
+	// test99
+	// test100
+	os.Args = backup
 }
