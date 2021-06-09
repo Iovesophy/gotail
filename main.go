9c3580bc01ce587b366c44a4b8a9df2174354cae
Iovesophy <15680172+Iovesophy@users.noreply.github.com>
2021-06-09 15:54:50 +0900

    FIX separate flag section on main

--- main.go
+++ main.go
@@ -30,7 +30,6 @@ type fileTail struct {
 }
 
 func (s *stdinTail) appendQueue(stream *os.File) {
-	defer stream.Close()
 	scanner := bufio.NewScanner(stream)
 	s.queue = make([]string, 0, s.maxQueueSize+1)
 	for scanner.Scan() {
@@ -61,11 +60,6 @@ func (f *fileTail) printTail() {
 	}
 }
 
-func doTail(t tailer, stream *os.File) {
-	t.appendQueue(stream)
-	t.printTail()
-}
-
 func xOpen(filename string) *os.File {
 	stream, err := os.Open(filename)
 	if err != nil {
@@ -75,15 +69,23 @@ func xOpen(filename string) *os.File {
 	return stream
 }
 
-func main() {
+func doTail(t tailer, stream *os.File) {
+	t.appendQueue(stream)
+	t.printTail()
+}
+
+func parseCLine() (*flag.FlagSet, *int, int) {
 	nFlags := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	nLines := nFlags.Int("n", defaultNLines, "number of lines")
 	nFlags.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s [-n #] [file]\n", os.Args[0])
 	}
 	nFlags.Parse(os.Args[1:])
-
 	nArg := nFlags.NArg()
+	return nFlags, nLines, nArg
+}
+
+func do(nFlags *flag.FlagSet, nLines *int, nArg int) {
 	if nArg > 0 {
 		for i := 0; i < nArg; i++ {
 			t := &fileTail{
@@ -94,10 +96,16 @@ func main() {
 					maxQueueSize: *nLines,
 				},
 			}
-			doTail(t, xOpen(t.filename))
+			stream := xOpen(t.filename)
+			defer stream.Close()
+			doTail(t, stream)
 		}
 	} else {
 		t := &stdinTail{maxQueueSize: *nLines}
 		doTail(t, os.Stdin)
 	}
+}
+
+func main() {
+	do(parseCLine())
 }
